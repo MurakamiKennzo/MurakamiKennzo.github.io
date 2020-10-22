00ab746ec9890a6d663f3f4e10c1c9a26f87e018
wangzilin <wangzilin@hupu.com>
2020-10-22 13:33:09 +0800

    generate.sh

--- generate.sh
+++ generate.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-if [ ! $0 ]; then
-    $0='publish'
-fi
-stack run -- html > index.html &&
+stack run -- html > index.html
 stack run -- style > index.css
-git checkout master &&
-git add . &&
-git commit -m $0 &&
-git push origin master &&
-git checkout hs-page
+
